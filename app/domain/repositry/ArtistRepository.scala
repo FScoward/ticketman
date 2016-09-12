aca56c05bf371d2a7f13dbf23c460c79cdc55c29
FScoward <endlick@gmail.com>
2016-09-13 00:59:32 +0900

    add repositry

---
+++ app/domain/repositry/ArtistRepository.scala
@@ -0,0 +1,8 @@
+package domain.repositry
+
+/**
+  * Created by Fumiyasu on 2016/09/13.
+  */
+trait ArtistRepository {
+
+}
---
+++ app/infrastructure/ArtistRepository.scala
@@ -0,0 +1,8 @@
+package infrastructure
+
+/**
+  * Created by Fumiyasu on 2016/09/13.
+  */
+class ArtistRepository {
+
+}
